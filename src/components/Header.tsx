100bc1cd5a225ebd58221fecf7212f41731372f9
NathanPip <107423247+NathanPip@users.noreply.github.com>
2022-10-31 12:42:50 -0700

    socials in nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -68,7 +68,7 @@ const Header: Component = () => {
       <ul className="nav">
             <a href="#projects"><li id="project-nav">projects</li></a>
             <a href="#skills"><li id="skill-nav">skills</li></a>
-            {/* <a href="#socials"><li id="project-nav">socials</li></a> */}
+            <a href="#socials"><li id="project-nav">socials</li></a>
             <a href="#contact"><li id="contact-nav">contact</li></a>
       </ul>
 
